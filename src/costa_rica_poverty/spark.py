bfc0d081b2d54a2b392f5850a91a78670bef88a7
Henry Fung <henryhfung21@gmail.com>
2024-12-24 22:23:49 -0500

    Init commit

---
+++ src/costa_rica_poverty/spark.py
@@ -0,0 +1,12 @@
+# from databricks.connect import DatabricksSession
+
+from costa_rica_poverty.config.core import config
+
+# from ptb_ltc.config.env import DB_CLUSTER_ID, DB_HOST, DB_TOKEN
+
+# spark = DatabricksSession.builder.profile(config.general.DB_PROFILE_NAME).getOrCreate()
+# spark = DatabricksSession.builder.remote(
+#    host=DB_HOST,
+#    token=DB_TOKEN,
+#    cluster_id=DB_CLUSTER_ID,
+# ).getOrCreate()
